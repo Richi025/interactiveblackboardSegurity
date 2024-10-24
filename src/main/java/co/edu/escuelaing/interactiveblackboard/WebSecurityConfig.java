9302d331c7c6370c2685bad15ef03da4489924b2
unknown <jose.vasquez-v@labinfo.is.escuelaing.edu.co>
2024-10-24 17:16:25 -0500

    intento seguridad 5

--- src/main/java/co/edu/escuelaing/interactiveblackboard/WebSecurityConfig.java
+++ src/main/java/co/edu/escuelaing/interactiveblackboard/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package co.edu.escuelaing.interactiveblackboard;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    // Inyectar los valores desde application.properties
+    @Value("${user1.username}")
+    private String user1Username;
+
+    @Value("${user1.password}")
+    private String user1Password;
+
+    @Value("${user2.username}")
+    private String user2Username;
+
+    @Value("${user2.password}")
+    private String user2Password;
+
+    @Value("${user3.username}")
+    private String user3Username;
+
+    @Value("${user3.password}")
+    private String user3Password;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,23 +50,24 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withDefaultPasswordEncoder()
-            .username("user1")
-            .password("Escuelaing.edu***123")
+            .username(user1Username)
+            .password(user1Password)
             .roles("USER")
             .build();
 
         UserDetails user2 = User.withDefaultPasswordEncoder()
-            .username("user2")
-            .password("password2")
+            .username(user2Username)
+            .password(user2Password)
             .roles("USER")
             .build();
 
         UserDetails user3 = User.withDefaultPasswordEncoder()
-            .username("user3")
-            .password("password3")
+            .username(user3Username)
+            .password(user3Password)
             .roles("USER")
             .build();
 
